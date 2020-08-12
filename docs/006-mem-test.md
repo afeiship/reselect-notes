c78ccb78c0de6df190a8dcaeec2842c1a3131086
afeiship <1290657123@qq.com>
2020-08-12 21:06:17 +0800

    add: new mem test results

---
+++ docs/006-mem-test.md
@@ -0,0 +1,12 @@
+# mem-test
+
+## 测试结果
+~~~
+{ total: 2.322 }
+{ total: 2.322 }
+reselect version: 0.241ms
+my version: 14.669ms
+~~~
+
+1. `reselect` 库在项目中的优势比较明显
+2. 刚开始做了一个很小的 `demo` ，完全没发现优势
--- src/002-mem-test.js
+++ src/002-mem-test.js
@@ -6,6 +6,12 @@ const {
 } = require('reselect');
 
 const memoize = require('lodash.memoize');
+const heaveCalc = () => {
+  var str = 'aaa';
+  for (let i = 0; i < 1000; i++) {
+    str += i;
+  }
+};
 // const mySelecotr = createSelectorCreator(memoize, );
 
 const exampleState = {
@@ -19,8 +25,8 @@ const exampleState = {
 };
 
 const shopItemsSelector = (state) => {
-  console.log('shop item selector call only once if state not changed');
-  return state.shop.items;;
+  // console.log('shop item selector call only once if state not changed');
+  return state.shop.items;
 };
 const taxPercentSelector = (state) => state.shop.taxPercent;
 
@@ -34,39 +40,41 @@ const taxSelector = createSelector(
   (subtotal, taxPercent) => subtotal * (taxPercent / 100)
 );
 
-const totalSelector = createSelector(subtotalSelector, taxSelector, (subtotal, tax) => ({
-  total: subtotal + tax
-}));
+const totalSelector = createSelector(subtotalSelector, taxSelector, (subtotal, tax) => {
+  // console.log('total selector');
+  heaveCalc();
+  return {
+    total: subtotal + tax
+  };
+});
 
 const myTotalWithoutReselect = (exampleState) => {
+  // console.log('my total.'); // will output everytime
   const subtotal = exampleState.shop.items.reduce((acc, item) => acc + item.value, 0);
-  const total = subtotal * (exampleState.shop.taxPercent / 100) + subtotal;;
-  return { total }
+  const total = subtotal * (exampleState.shop.taxPercent / 100) + subtotal;
+  heaveCalc();
+  return { total };
 };
+
+
 
 console.log(totalSelector(exampleState)); // { total: 2.322 }
 console.log(myTotalWithoutReselect(exampleState)); // { total: 2.322 }
 
-
-const times = 10000;
-
+const times = 1000;
 
 /**
  * Cached? Slow?
  */
 
-
-console.time('reselect version')
+console.time('reselect version');
 for (let i = 0; i < times; i++) {
   totalSelector(exampleState);
 }
 console.timeEnd('reselect version');
-
-
 
 console.time('my version');
 for (let i = 0; i < times; i++) {
   myTotalWithoutReselect(exampleState);
 }
 console.timeEnd('my version');
-
