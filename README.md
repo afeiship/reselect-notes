a5fda454aca31c0310da686c3bb188ac257371a2
afeiship <1290657123@qq.com>
2020-08-12 15:46:15 +0800

    add: get-started example

---
+++ README.md
@@ -0,0 +1,5 @@
+# reselect-notes
+> Reselect notes.
+
+## resources
+- https://github.com/reduxjs/reselect
---
+++ src/001-get-started.js
@@ -0,0 +1,37 @@
+const {
+  defaultMemoize,
+  createSelectorCreator,
+  createStructuredSelector,
+  createSelector
+} = require('reselect');
+
+const exampleState = {
+  shop: {
+    taxPercent: 8,
+    items: [
+      { name: 'apple', value: 1.2 },
+      { name: 'orange', value: 0.95 }
+    ]
+  }
+};
+
+const shopItemsSelector = (state) => state.shop.items;
+const taxPercentSelector = (state) => state.shop.taxPercent;
+
+const subtotalSelector = createSelector(shopItemsSelector, (items) =>
+  items.reduce((acc, item) => acc + item.value, 0)
+);
+
+const taxSelector = createSelector(
+  subtotalSelector,
+  taxPercentSelector,
+  (subtotal, taxPercent) => subtotal * (taxPercent / 100)
+);
+
+const totalSelector = createSelector(subtotalSelector, taxSelector, (subtotal, tax) => ({
+  total: subtotal + tax
+}));
+
+console.log(subtotalSelector(exampleState)); // 2.15
+console.log(taxSelector(exampleState)); // 0.172
+console.log(totalSelector(exampleState)); // { total: 2.322 }
